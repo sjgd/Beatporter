3a0355c52727c0cebcf6bbd6ee5c71cc9e4cbbb1
sjgd <sylvain.jeangirard@gmail.com>
2022-04-23 08:29:28 +0200

    Add arguments

--- beatporter.py
+++ beatporter.py
@@ -1,12 +1,14 @@
+import sys
+from os import path
 import spotify
 import beatport
 from datetime import datetime
-from os import path
 import pandas as pd
 from config import username, shuffle_label, daily_mode, daily_n_track, folder_path
 from time import sleep
 import openpyxl
 import random
+#import argparse
 import logging
 logging.getLogger().setLevel(logging.INFO)
 logging.basicConfig(format='%(message)s')
@@ -14,6 +16,7 @@ logging.basicConfig(format='%(message)s')
 
 file_name_hist = 'hist_playlists_tracks.pkl'
 curr_date = datetime.today().strftime('%Y-%m-%d')
+option_parse = ['backup', 'chart', 'genre', 'label']
 
 
 def dump_tracks(tracks):
@@ -71,43 +74,54 @@ def main():
     df_hist_pl_tracks = load_hist_file()
     beatport.charts = {beatport.parse_chart_url_datetime(k): beatport.parse_chart_url_datetime(v) for k, v in beatport.charts.items()}
 
+    # Load arguments
+    args = sys.argv[1:]
+    args = [arg.replace('-', '') for arg in args]
+    if len(args) == 0:
+        # If not argument passed then parse all
+        args = option_parse
+   
     # if path.exists(folder_path+file_name_hist):
     #     df_hist_pl_tracks = pd.read_pickle(folder_path+file_name_hist)
     # else:
     #     df_hist_pl_tracks = pd.DataFrame(columns=['playlist_id', 'playlist_name', 'track_id', 'datetime_added', 'artist_name'])
 
-    for playlist_name, org_playlist_id in beatport.spotify_bkp.items():
-        logging.info("\n Backing up to playlist : ***** {} : {} *****".format(playlist_name, org_playlist_id))
-        df_hist_pl_tracks = spotify.back_up_spotify_playlist(playlist_name, org_playlist_id, df_hist_pl_tracks)
+    if 'backup' in args:
+        for playlist_name, org_playlist_id in beatport.spotify_bkp.items():
+            logging.info("\n Backing up to playlist : ***** {} : {} *****".format(playlist_name, org_playlist_id))
+            df_hist_pl_tracks = spotify.back_up_spotify_playlist(playlist_name, org_playlist_id, df_hist_pl_tracks)
 
     # Parse lists
-    for chart, chart_bp_url_code in beatport.charts.items():
-        #TODO handle return None, handle chart_bp_url_code has ID already or not
-        logging.info("\n Getting chart : ***** {} : {} *****".format(chart, chart_bp_url_code))
-        chart_url = beatport.find_chart(chart_bp_url_code)
+    if 'chart' in args:
+        for chart, chart_bp_url_code in beatport.charts.items():
+            #TODO handle return None, handle chart_bp_url_code has ID already or not
+            logging.info("\n Getting chart : ***** {} : {} *****".format(chart, chart_bp_url_code))
+            chart_url = beatport.find_chart(chart_bp_url_code)
 
-        if chart_url:
-            tracks_dict = beatport.get_chart(beatport.find_chart(chart_bp_url_code))
-            logging.info("\t[+] Found {} tracks for {}".format(len(tracks_dict), chart))
-        else:
-            logging.info("\t[+] Chart not found")
+            if chart_url:
+                tracks_dict = beatport.get_chart(beatport.find_chart(chart_bp_url_code))
+                logging.info("\t[+] Found {} tracks for {}".format(len(tracks_dict), chart))
+            else:
+                logging.info("\t[+] Chart not found")
 
-        df_hist_pl_tracks = spotify.add_new_tracks_to_playlist_chart_label(chart, tracks_dict, df_hist_pl_tracks)
+            df_hist_pl_tracks = spotify.add_new_tracks_to_playlist_chart_label(chart, tracks_dict, df_hist_pl_tracks)
 
-    for genre, genre_bp_url_code in beatport.genres.items():
-        logging.info("\n Getting genre : ***** {} *****".format(genre))
-        top_100_chart = beatport.get_top_100_tracks(genre)
-        df_hist_pl_tracks = spotify.add_new_tracks_to_playlist_genre(genre, top_100_chart, df_hist_pl_tracks)
+    if 'genre' in args:
+        for genre, genre_bp_url_code in beatport.genres.items():
+            logging.info("\n Getting genre : ***** {} *****".format(genre))
+            top_100_chart = beatport.get_top_100_tracks(genre)
+            df_hist_pl_tracks = spotify.add_new_tracks_to_playlist_genre(genre, top_100_chart, df_hist_pl_tracks)
 
-    for label, label_bp_url_code in beatport.labels.items():
-        # TODO avoid looping through all pages if already parsed before ?
-        # TODO Add tracks per EP rather than track by track ?
-        logging.info("\n Getting label : ***** {} : {} *****".format(label, label_bp_url_code))
-        tracks_dict = beatport.get_label_tracks(label, label_bp_url_code, df_hist_pl_tracks)
-        logging.info("Found {} tracks for {}".format(len(tracks_dict), label))
-        if shuffle_label:
-            random.shuffle(tracks_dict)
-        df_hist_pl_tracks = spotify.add_new_tracks_to_playlist_chart_label(label, tracks_dict, df_hist_pl_tracks)
+    if 'label' in args:
+        for label, label_bp_url_code in beatport.labels.items():
+            # TODO avoid looping through all pages if already parsed before ?
+            # TODO Add tracks per EP rather than track by track ?
+            logging.info("\n Getting label : ***** {} : {} *****".format(label, label_bp_url_code))
+            tracks_dict = beatport.get_label_tracks(label, label_bp_url_code, df_hist_pl_tracks)
+            logging.info("Found {} tracks for {}".format(len(tracks_dict), label))
+            if shuffle_label:
+                random.shuffle(tracks_dict)
+            df_hist_pl_tracks = spotify.add_new_tracks_to_playlist_chart_label(label, tracks_dict, df_hist_pl_tracks)
 
     # Output
     logging.info("\n Saving file")
